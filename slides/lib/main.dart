d302da0abd024afc99d8c7ad01fdaeb251df30ea
Roaa94 <roaa94khaddam@gmail.com>
2024-06-23 11:54:05 +0200

    feat(slides): set up with text theming workaround for custom font

--- slides/lib/main.dart
+++ slides/lib/main.dart
@@ -1,20 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_deck/flutter_deck.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const SlidesApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SlidesApp extends StatelessWidget {
+  const SlidesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return FlutterDeckApp(
+      themeMode: ThemeMode.dark,
+      speakerInfo: const FlutterDeckSpeakerInfo(
+        name: 'Roaa Khaddam',
+        description: 'Software Engineer, Flutter & Dart GDE',
+        imagePath: 'assets/images/profile-pic-min.png',
+        socialHandle: '@roaakdm',
+      ),
+      configuration: const FlutterDeckConfiguration(
+        showProgress: false,
+        controls: FlutterDeckControlsConfiguration(
+          presenterToolbarVisible: true,
         ),
       ),
+      slides: [
+        PlaceholderSlide('Splash Demo Slide'),
+        PlaceholderSlide('Title (with splash demo as dimmed bg)'),
+        PlaceholderSlide('Introduction'),
+      ],
+    );
+  }
+}
+
+class PlaceholderSlide extends FlutterDeckSlideWidget {
+  PlaceholderSlide(this.title)
+      : super(
+          configuration: FlutterDeckSlideConfiguration(
+            route: '/${title.toLowerCase().replaceAll(' ', '')}',
+            title: title,
+          ),
+        );
+
+  final String title;
+
+  @override
+  FlutterDeckSlide build(BuildContext context) {
+    return FlutterDeckSlide.custom(
+      builder: (context) => SlideTextThemeWrapper(
+        child: PlaceholderSlideContent(title),
+      ),
+    );
+  }
+}
+
+class PlaceholderSlideContent extends StatelessWidget {
+  const PlaceholderSlideContent(this.title, {super.key});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headlineLarge,
+      ),
+    );
+  }
+}
+
+class SlideTextThemeWrapper extends StatelessWidget {
+  const SlideTextThemeWrapper({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        textTheme: TextTheme(
+          titleLarge: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(fontFamily: 'Poppins'),
+          headlineLarge: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                fontFamily: 'Poppins',
+                fontSize: 50,
+                fontWeight: FontWeight.w700,
+              ),
+        ),
+      ),
+      child: child,
     );
   }
 }
