d0fa28489e4d9d1b1b8a6512039b8f52773ee51a
Roaa94 <roaa94khaddam@gmail.com>
2024-07-07 10:37:38 +0200

    feat(slides): remove splash slide demo

--- slides/lib/slides/00-introduction/slides.dart
+++ slides/lib/slides/00-introduction/slides.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_deck/flutter_deck.dart';
-import 'package:slides/slides/00-introduction/splash_slide.dart';
 import 'package:slides/slides/00-introduction/title_slide.dart';
 
 final introductionSlides = <FlutterDeckSlideWidget>[
-  // Todo: consider running the app separately for the splash demo to avoid crashes
-  SplashSlide(),
   const TitleSlide(),
 ];
