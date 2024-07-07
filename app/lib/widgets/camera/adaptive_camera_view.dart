9fa476e6a038f36a842e42a8118e92c6513201a3
Roaa94 <roaa94khaddam@gmail.com>
2024-07-07 14:06:45 +0200

    refactor(app): use repaint boundary instead of image stream

--- app/lib/widgets/camera/adaptive_camera_view.dart
+++ app/lib/widgets/camera/adaptive_camera_view.dart
@@ -1,12 +1,11 @@
 import 'dart:developer';
 import 'dart:typed_data';
+import 'dart:ui' as ui;
 
-import 'package:app/utils.dart';
 import 'package:camera_macos/camera_macos.dart';
 import 'package:flutter/material.dart';
-
-const videoWidth = 960.0;
-const videoHeight = 540.0;
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
 
 class AdaptiveCameraView extends StatefulWidget {
   const AdaptiveCameraView({
@@ -20,23 +19,24 @@ class AdaptiveCameraView extends StatefulWidget {
   State<AdaptiveCameraView> createState() => _AdaptiveCameraViewState();
 }
 
-class _AdaptiveCameraViewState extends State<AdaptiveCameraView> {
+class _AdaptiveCameraViewState extends State<AdaptiveCameraView>
+    with SingleTickerProviderStateMixin {
+  late final Ticker _ticker;
+
   CameraMacOSController? macOSController;
   GlobalKey cameraKey = GlobalKey();
   GlobalKey cameraRenderBoxKey = GlobalKey();
   List<CameraMacOSDevice> _videoDevices = [];
   String? _selectedVideoDeviceId;
 
-  Future<void> _loadPixelsFromStreamedImage(
-    CameraImageData? streamedImage,
-  ) async {
-    if (streamedImage != null) {
-      var decodedImage =
-          await decodeImageFromList(argb2bitmap(streamedImage).bytes);
-      final imageBytes = await decodedImage.toByteData();
-      if (imageBytes != null) {
-        widget.imageStream(imageBytes);
-      }
+  Future<void> _loadPixelsFromRepaintBoundary() async {
+    if (cameraRenderBoxKey.currentContext == null) return;
+    RenderRepaintBoundary boundary = cameraRenderBoxKey.currentContext!
+        .findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData? imageBytes = await image.toByteData();
+    if (imageBytes != null) {
+      widget.imageStream(imageBytes);
     }
   }
 
@@ -56,10 +56,22 @@ class _AdaptiveCameraViewState extends State<AdaptiveCameraView> {
     }
   }
 
+  void _handleSelectVideoDevice(CameraMacOSDevice device) {
+    setState(() {
+      _selectedVideoDeviceId = device.deviceId;
+    });
+    _ticker.start();
+  }
+
+  void _onTick(Duration elapsed) {
+    _loadPixelsFromRepaintBoundary();
+  }
+
   @override
   void initState() {
     super.initState();
     _listVideoDevices();
+    _ticker = createTicker(_onTick);
   }
 
   @override
@@ -67,7 +79,6 @@ class _AdaptiveCameraViewState extends State<AdaptiveCameraView> {
     super.dispose();
     if (macOSController != null && !macOSController!.isDestroyed) {
       log('Disposing camera...');
-      macOSController!.stopImageStream();
       macOSController!.destroy();
     }
   }
@@ -75,63 +86,62 @@ class _AdaptiveCameraViewState extends State<AdaptiveCameraView> {
   @override
   Widget build(BuildContext context) {
     if (_selectedVideoDeviceId == null) {
-      return Center(
+      return Padding(
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: _videoDevices
-              .map(
-                (device) => GestureDetector(
-                  onTap: () => setState(() {
-                    _selectedVideoDeviceId = device.deviceId;
-                  }),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 12,
-                      vertical: 8,
-                    ),
-                    margin: const EdgeInsets.symmetric(vertical: 5),
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Text(
-                      device.localizedName ?? device.deviceId,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Poppins',
-                        fontSize: 20,
-                      ),
-                    ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: List.generate(
+            _videoDevices.length,
+            (i) => GestureDetector(
+              onTap: () => _handleSelectVideoDevice(_videoDevices[i]),
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+                margin: const EdgeInsets.symmetric(vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  _videoDevices[i].localizedName ?? _videoDevices[i].deviceId,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Poppins',
+                    fontSize: 20,
                   ),
                 ),
-              )
-              .toList(),
+              ),
+            ),
+          ),
         ),
       );
     }
 
-    return CameraMacOSView(
-      key: cameraKey,
-      deviceId: _selectedVideoDeviceId,
-      fit: BoxFit.cover,
-      cameraMode: CameraMacOSMode.photo,
-      resolution: PictureResolution.medium,
-      pictureFormat: PictureFormat.tiff,
-      videoFormat: VideoFormat.mp4,
-      onCameraInizialized: (CameraMacOSController controller) {
-        setState(() {
-          macOSController = controller;
-        });
-        macOSController?.startImageStream((image) {
-          _loadPixelsFromStreamedImage(image);
-        });
-      },
-      onCameraDestroyed: () {
-        log('Camera destroyed!');
-        return const Text('Camera Destroyed!');
-      },
-      toggleTorch: Torch.off,
-      enableAudio: false,
+    return RepaintBoundary(
+      key: cameraRenderBoxKey,
+      child: CameraMacOSView(
+        key: cameraKey,
+        deviceId: _selectedVideoDeviceId,
+        fit: BoxFit.cover,
+        cameraMode: CameraMacOSMode.photo,
+        resolution: PictureResolution.medium,
+        pictureFormat: PictureFormat.tiff,
+        videoFormat: VideoFormat.mp4,
+        onCameraInizialized: (CameraMacOSController controller) {
+          setState(() {
+            macOSController = controller;
+          });
+        },
+        onCameraDestroyed: () {
+          log('Camera destroyed!');
+          return const Text('Camera Destroyed!');
+        },
+        toggleTorch: Torch.off,
+        enableAudio: false,
+      ),
     );
   }
 }
